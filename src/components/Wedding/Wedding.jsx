454940e1be7916a5d9ca0c57a36d00f62054cc9a
codedcollections <ma.unander@gmail.com>
2026-01-23 19:38:52 +0100

    Add component Wedding

---
+++ src/components/Wedding/Wedding.jsx
@@ -0,0 +1,8 @@
+const Wedding = () => {
+  return (
+    <div id={s["invitation"]}>
+      <h2>Vigsel</h2>
+    </div>
+  )
+}
+export default Wedding
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,14 @@
 import s from "./HomePage.module.css"
 import Introduction from "../../components/Introduction/Introduction"
 import InvitationForm from "../../components/InvitationForm/InvitationForm"
+import Wedding from "../../components/Wedding/Wedding"
 
 const HomePage = () => {
   return (
     <div className={`${s["wrapper"]} flex`}>
       <Introduction />
       <InvitationForm />
+      <Wedding />
       <p id="navigationtester" className={s.added}>
         end of website
       </p>
