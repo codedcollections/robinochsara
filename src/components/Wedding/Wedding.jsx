c8a5997691faefec10d914bffc1f170017ac99c1
codedcollections <ma.unander@gmail.com>
2026-03-03 15:13:48 +0100

    Update img source

--- src/components/Wedding/Wedding.jsx
+++ src/components/Wedding/Wedding.jsx
@@ -15,6 +15,13 @@ const Wedding = () => {
         att ta sig till kyrkan med kollektivtrafik, men det finns även
         parkeringar precis vid kyrkan.
       </p>
+      <p className={`flex-align-self-start ${s.imgsource}`}>
+        Källa bild: "Huddinge kyrka" av Jssfrk, via Wikimedia Commons, licens
+        <a href=" https://creativecommons.org/licenses/by-sa/3.0/">
+          CC BY-SA 3.0
+        </a>
+        .
+      </p>
       <p>
         {/*Sundby gård:
          "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d127.67847005353346!2d18.021793251426015!3d59.201713330771184!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465f7a7a8514047b%3A0xb8c9412b5bcc2b81!2sSundby%20g%C3%A5rd!5e0!3m2!1ssv!2sse!4v1767550849941!5m2!1ssv!2sse" 
