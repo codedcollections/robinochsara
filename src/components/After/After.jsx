69b1c9bd8301603466de1aa871f5bb56c2d4d101
codedcollections <ma.unander@gmail.com>
2026-04-07 08:24:49 +0200

    remove duplicate div in after and dinner component

--- src/components/After/After.jsx
+++ src/components/After/After.jsx
@@ -2,18 +2,16 @@ import s from "./After.module.css"
 import after from "./../../images/table1.png"
 const After = () => {
   return (
-    <div>
-      <div id={s["after"]} className={`flex flex-down`}>
-        <h2></h2>
-        <img src={after} alt="placeholder image" />
-        <div>
-          <p>
-            När Sundby Gård stänger för kvällen så kan man kliva på bussen som
-            kör till en efterfest med hemlig adress i Huddinge med närhet till
-            nattbuss. För er som gått barfota i skorna under dagen kan det vara
-            skönt att ha med sig ett par strumpor.
-          </p>
-        </div>
+    <div id={s["after"]} className={`flex flex-down`}>
+      <h2></h2>
+      <img src={after} alt="placeholder image" />
+      <div>
+        <p>
+          När Sundby Gård stänger för kvällen så kan man kliva på bussen som kör
+          till en efterfest med hemlig adress i Huddinge med närhet till
+          nattbuss. För er som gått barfota i skorna under dagen kan det vara
+          skönt att ha med sig ett par strumpor.
+        </p>
       </div>
     </div>
   )
--- src/components/Dinner/Dinner.jsx
+++ src/components/Dinner/Dinner.jsx
@@ -2,25 +2,23 @@ import s from "./Dinner.module.css"
 import dinnerPlace from "./../../images/SundbyMiddag.jpg"
 const Dinner = () => {
   return (
-    <div>
-      <div id={s["dinner"]} className={`flex flex-down`}>
-        <img src={dinnerPlace} alt="" />
-        <h2>Middag vid Sundby Gård</h2>
+    <div id={s["dinner"]} className={`flex flex-down`}>
+      <img src={dinnerPlace} alt="" />
+      <h2>Middag vid Sundby Gård</h2>
 
-        {/* <PlaceDetails imageObj={dinnerPlace} position={[59.2016, 18.0215]} /> */}
-        <div>
-          <p>
-            Middagen äger rum på Sundby Gård. Firandet på Sundby Gård börjar med
-            brudskål och avslutas med dans efter en tre-rätters middag.
-          </p>
-          <a
-            className={s.dinneraddress}
-            href="https://maps.app.goo.gl/mmTV71bycjAnee8b7"
-          >
-            Se karta{/* Sundby Gårdsväg 5, 141 91 Huddinge */}
-          </a>
-          <p></p>
-        </div>
+      {/* <PlaceDetails imageObj={dinnerPlace} position={[59.2016, 18.0215]} /> */}
+      <div>
+        <p>
+          Middagen äger rum på Sundby Gård. Firandet på Sundby Gård börjar med
+          brudskål och avslutas med dans efter en tre-rätters middag.
+        </p>
+        <a
+          className={s.dinneraddress}
+          href="https://maps.app.goo.gl/mmTV71bycjAnee8b7"
+        >
+          Se karta{/* Sundby Gårdsväg 5, 141 91 Huddinge */}
+        </a>
+        <p></p>
       </div>
     </div>
   )
