155515856ca1c0a2d243601bbedec2d969fb219c
codedcollections <ma.unander@gmail.com>
2026-04-28 19:16:41 +0200

    Update margin on all headers to be the same distance

--- src/components/Practicalities/Practicalities.jsx
+++ src/components/Practicalities/Practicalities.jsx
@@ -13,6 +13,7 @@ const Practicalities = () => {
         <PracticalityCard
           infoTitle={"Mat och dryck"}
           imageSrc={dinner}
+          imageAlt={`Ett elegant uppdukat bord`}
           infoText={
             "Det kommer serveras trerättersmiddag med dryck och traditionsenlig bröllopstårta.\nHar du allergier eller matpreferenser ska de meddelas i samband med din OSA.\nEfter middagen kommer det finns en bar för inköp av dryck.\nPå efterfesten serveras vickning och det kommer finns tillgång till en swishbar med öl, vin och cider."
           }
@@ -20,6 +21,7 @@ const Practicalities = () => {
         <PracticalityCard
           infoTitle={"Transport"}
           imageSrc={transport}
+          imageAlt={`En gul leksaksbuss med bagage på taket placerad bland markkrypande växter på en strand`}
           infoText={
             "Det kommer gå en buss till och från middagen. För att garantera plats på bussen krävs anmälan i samband med OSA.\nBussen kommer åka mellan Huddinge Kyrka och Sundby Gård.\nBussen kommer åka mellan Sundby Gård och Efterfesten med ett snabbt stopp vid Huddinge station. Det går att ta tåg från Huddinge station för de som har festat klart."
           }
@@ -27,11 +29,13 @@ const Practicalities = () => {
         <PracticalityCard
           infoTitle={"Klädsel"}
           imageSrc={dressCode}
+          imageAlt={`Personer i kostymer i och klänningar i olika färger och mönster`}
           infoText={`Den generella klädkoden är kostym, vilket innebär:\nKostym - i valfri färg och mönster på skjorta och kostym. Slips eller fluga kan vara i valfri färg och mönster.\nKlänning - i valfri färg och mönster som är till knäna eller längre. Alternativt byxdress eller kjol.\nMen det viktigaste är inte att följa klädkoden utan att du känner dig fin och bekväm i vad du har på dig.`}
         />
         <PracticalityCard
           infoTitle={"Barn"}
           imageSrc={children}
+          imageAlt={`Barnfötter intill byggklossar med djurmotiv`}
           infoText={
             <>
               Vi tycker jättemycket om era barn, men idag har vi valt att de får
--- src/components/PracticalityCard/PracticalityCard.jsx
+++ src/components/PracticalityCard/PracticalityCard.jsx
@@ -1,8 +1,8 @@
 import s from "./PracticalityCard.module.css"
-const PracticalityCard = ({ infoTitle, imageSrc, infoText }) => {
+const PracticalityCard = ({ infoTitle, imageSrc, imageAlt, infoText }) => {
   return (
     <div id={s["practicalcard"]} className="flex flex-down">
-      <img src={imageSrc} alt="image not found" />
+      <img src={imageSrc} alt={imageAlt} />
       <div id={s["cardtextinfo"]}>
         <h3>{infoTitle}</h3>
         <p className={s.multiline}>{infoText}</p>
